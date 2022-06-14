b6c6bf0de67a3b23efd7e3d58a85e18f18d550b2
Jonatas João <jojokl@hotmail.com>
2022-06-14 02:25:08 -0300

    Implement TravelRouteController

--- TravelRoute/Controllers/TravelRouteController.cs
+++ TravelRoute/Controllers/TravelRouteController.cs
@@ -4,13 +4,63 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TravelRouteService.Interfaces;
+using TravelRoutePersistence.Model;
 
 namespace TravelRoute.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("[controller]")]
     public class TravelRouteController : ControllerBase
     {
+        private readonly ITravelRouteService _travelRouteService;
+        public TravelRouteController(ITravelRouteService travelRouteService)
+        {
+            _travelRouteService =  travelRouteService;
+        }
 
+        [HttpGet("TravelRoutes")]
+        public IActionResult TravelRoutes()
+        {
+            try
+            {
+                var result = _travelRouteService.GetTravelRoutes();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.ToString());
+            }
+        }
+        [HttpGet("CheapestRoute")]
+        public IActionResult CheapestRoute(string origin, string destination)
+        {
+            try
+            {
+                var result = _travelRouteService.GetCheapestRoute(origin, destination);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.ToString());
+            }
+        }
+
+        [HttpPost("TravelRoute")]
+        public IActionResult TravelRoute(Route route)
+        {
+            try
+            {
+                bool result = _travelRouteService.AddTravelRoute(route);
+                if (result)
+                    return Ok();
+                else
+                    return BadRequest();
+            }
+            catch( Exception ex)
+            {
+                return Problem(ex.ToString());
+            }
+        }
     }
 }
