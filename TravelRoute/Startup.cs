80f1c07dcccc43d360c9f84571bd314ae7d00b0f
Jonatas João <jojokl@hotmail.com>
2022-06-14 02:25:59 -0300

    initialize services at startup.cs

--- TravelRoute/Startup.cs
+++ TravelRoute/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TravelRoutePersistence.DatabaseContext;
+using TravelRoutePersistence.Interfaces;
+using TravelRoutePersistence.Repository;
+using TravelRouteService.Interfaces;
 
 namespace TravelRoute
 {
@@ -26,6 +31,15 @@ namespace TravelRoute
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Database
+            services.AddDbContext<TravelRouteContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //Service
+            services.AddScoped<ITravelRouteService, TravelRouteService.Services.TravelRouteService>();
+
+            //Repository
+            services.AddScoped<ITravelRouteRepository, TravelRouteRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
